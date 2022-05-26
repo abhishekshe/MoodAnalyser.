a4c9e715c1e31c71d34d48ab9769c5a61ff3e1ca
Abhishek Narwariya <abhisheknarwariya813@gmail.com>
2022-05-26 14:52:09 +0530

    Adding UC2

--- MoodAnalyser/Program.cs
+++ MoodAnalyser/Program.cs
@@ -6,7 +6,11 @@ namespace MoodAnalyserAppWithCore
     {
         static void Main(string[] args)
         {
-           
+            Console.WriteLine("Welcome to Mood Analyser Problem");
+            MoodAnalyser moodAnalyserClass = new MoodAnalyser(null);
+            string mood = moodAnalyserClass.AnalyseMood();
+            Console.WriteLine(mood);
+
         }
     }
 }
