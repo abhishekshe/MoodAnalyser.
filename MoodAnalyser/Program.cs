578c4cf439698f46da9c4b6ee1a2ea9ee8d5f347
Abhishek Narwariya <abhisheknarwariya813@gmail.com>
2022-05-27 16:32:46 +0530

    Adding UC6

--- MoodAnalyser/Program.cs
+++ MoodAnalyser/Program.cs
@@ -8,15 +8,17 @@ namespace MoodAnalyserAppWithCore
         {
             try
             {
-                Console.WriteLine("Welcome to Mood Analyser Problem");
-                MoodAnalyser moodAnalyserClass = new MoodAnalyser(null);
-                string mood = moodAnalyserClass.AnalyseMood();
-                Console.WriteLine(mood);
+                //Console.WriteLine("Welcome to Mood Analyser Problem");
+                //MoodAnalyser moodAnalyserClass = new MoodAnalyser(null);
+                //string mood = moodAnalyserClass.AnalyseMood();
+                //Console.WriteLine(mood);
+
+                object moodAnalyserClass = MoodAnalyserFactory.CreateMoodAnalyse("MoodAnalyser.MoodAnalyserClass", "MoodAnalyserClass");
             }
             catch (MoodAnalyserCustomException ex)
             {
                 Console.WriteLine(ex.GetType().Name + ex.Message);
-                Console.WriteLine(ex);
+                //Console.WriteLine(ex);
 
             }
         }
